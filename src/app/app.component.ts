98837403d7db015a6ee0fcc4da5d8ee99e3c4306
manumitsacademy <manumits.info@gmail.com>
2021-08-06 11:52:11 +0530

    'todolist'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { task } from './task';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myapp';
+  tasks:task[]=[];
+  newtask:task={title:'',status:false};
+  addTask(){
+    this.tasks.push(this.newtask);
+    this.newtask={title:'',status:false};
+  }
+  editTask(i:number){
+    var newtitle = prompt("Enter the updated tasked");
+    this.tasks[i].title=newtitle;    
+  }
+  doneTask(i:number){
+    this.tasks[i].status=!this.tasks[i].status;
+  }
+  deleteTask(i:number){
+    this.tasks.splice(i,1);
+  }
 }
